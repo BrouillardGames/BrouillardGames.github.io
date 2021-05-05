4a69b7580e53f9c2363a9e5e0cf8ea57ca62466e
BrouillardGames <83727172+BrouillardGames@users.noreply.github.com>
2021-05-06 04:57:38 +0500

    Update game.js

--- BuggyDrive/Game/game.js
+++ BuggyDrive/Game/game.js
@@ -1,3 +1,5 @@
+var vk_user_id;
+
 VK.init(function() {
 	init();
   }, function() {
@@ -6,12 +8,12 @@ VK.init(function() {
 }, '5.130');
 
 function init() {
-	
+	VK.api("user.get", {"fields": "id!", "v":"5.73"}, function (data) { vk_user_id = data.response[0].id });
 }
 
 
 function post(score) {
-	VK.api("wall.post", {"message": "Я набрал " + (score) + " очков в игре Buggy Drive(https://vk.com/app7846841). Сможешь больше? #BrouillardGames", "attachments" : "photo-165410964_457239179, https://vk.com/app7846841", "v":"5.73"}, function (data) {
+	VK.api("wall.post", {"message": "Я набрал " + (score) + " очков в игре Buggy Drive (https://vk.com/app7846841). Сможешь больше? #BrouillardGames", "attachments" : "photo-165410964_457239179, https://vk.com/app7846841", "v":"5.73"}, function (data) {
 });
 }
 	
@@ -19,3 +21,52 @@ function friend() {
 	VK.callMethod("showInviteBox");
 }
 
+/*
+
+window.addEventListener('load', function() {
+ 
+   var user_id = vk_user_id;   // user's id
+   var app_id = 7846841;  // your app's id
+ 
+   admanInit({
+   user_id: user_id,
+   app_id: 7846841,
+   type: 'preloader'         // 'preloader' or 'rewarded' (default - 'preloader')
+   // params: {preview: 1}   // to verify the correct operation of advertising
+}, onAdsReady, onNoAds);
+ 
+function onAdsReady(adman) {
+    adman.onStarted(function () {});
+    adman.onCompleted(function() {});          
+    adman.onSkipped(function() {});          
+    adman.onClicked(function() {});
+    adman.start('preroll');
+};
+function onNoAds() {};
+
+*/
+
+setTimeout(() => {
+   ShowAdvert();
+}, 10000);
+
+function() ShowAdvert {
+ 
+   var user_id = vk_user_id;   // user's id
+   var app_id = 7846841;  // your app's id
+ 
+   admanInit({
+   user_id: user_id,
+   app_id: 7846841,
+   type: 'preloader'         // 'preloader' or 'rewarded' (default - 'preloader')
+   // params: {preview: 1}   // to verify the correct operation of advertising
+}, onAdsReady, onNoAds);
+ 
+function onAdsReady(adman) {
+    adman.onStarted(function () {});
+    adman.onCompleted(function() {});          
+    adman.onSkipped(function() {});          
+    adman.onClicked(function() {});
+    adman.start('preroll');
+};
+function onNoAds() {};
